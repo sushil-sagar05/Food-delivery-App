bacd4a1df6221198759dc2863850f230af13763f
sushil-sagar05 <sagarsushil1403@gmail.com>
2025-01-17 01:55:26 +0530

    Fixed cart for smaller screens

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -8,14 +8,14 @@ import { clearCart } from '../Redux/Slices/CartSlice';
 
 function Cart() {
   const dispatch = useDispatch()
-  const[activeCart, setactiveCart] = useState(false)
+  const [activeCart, setactiveCart] = useState(false)
   const cartItems = useSelector((state) => state.cart.cart)
   const totalqty = cartItems.reduce((totalqty, item) => totalqty + item.qty, 0 )
   const totalPrice = cartItems.reduce((totalPrice,item) => totalPrice+ item.qty * item.price, 0)
   const ClearCart = () => {dispatch(clearCart())}
   return (
     <>
-    <div className={`fixed p-5 right-0 top-0 w-[20vw] h-1/2 bg-white ${activeCart ? "translate-x-0" : "translate-x-full"} transition-all duration-500 z-50`}>
+    <div className={`fixed p-5 right-0 top-0 w-full sm:w-[20vw] h-full sm:h-full bg-white ${activeCart ? "translate-x-0" : "translate-x-full"} transition-all duration-500 z-50`}>
         <div className='flex justify-between items-center my-3'>
             <span className='text-xl font-bold text-gray-800'>My Order</span>
             <IoMdClose 
@@ -24,18 +24,17 @@ function Cart() {
         </div>
       {cartItems.length > 0 ? cartItems.map((food) => {
         return <ItemCard key={food.id} id= {food.id} name={food.name} price={food.price} img={food.img} qty={food.qty}/>
-      }) : <h2 className='text-center text-xl font-bold text-gray-800' >Your Card is empty </h2>}
+      }) : <h2 className='text-center text-xl font-bold text-gray-800' >Your Cart is empty </h2>}
         
-        <div className='absolute bottom-0'>
-            <h3 className='font-semibold text-gray-800'>Items:{totalqty} </h3>
-            <h3  className='font-semibold text-gray-800'> Total Ammount :{totalPrice} </h3>
-            <hr className='w-[18vw] my-2' />
-            <button className='bg-green-500 font-bold px-3 text-white py-2 rounded-lg w-[18vw] mb-5 '> Checkout</button>
+        <div className='absolute bottom-0 w-full sm:w-auto'>
+            <h3 className='font-semibold text-gray-800'>Items: {totalqty} </h3>
+            <h3 className='font-semibold text-gray-800'> Total Amount: {totalPrice} </h3>
+            <hr className='w-full sm:w-[18vw] my-2' />
+            <button className='bg-green-500 font-bold px-3 text-white py-2 rounded-lg w-full sm:w-[18vw] mb-5'> Checkout</button>
         </div>
-        {cartItems.length>0 ?  <button 
+        {cartItems.length > 0 ? <button 
            onClick={() => ClearCart()}
-               className='bg-green-500 font-bold px-3 text-white py-2 my-3 rounded-lg w-[8vw] mb-5'>Clear Cart</button> : ""}
-       
+           className='bg-green-500 font-bold px-3 text-white py-2 my-3 rounded-lg w-full sm:w-[8vw] mb-5'>Clear Cart</button> : ""}
     </div>
     <FaShoppingCart 
     onClick={()=> setactiveCart(!activeCart)}
